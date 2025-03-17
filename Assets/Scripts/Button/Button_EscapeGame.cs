c6bc20cdd6e8ce6d7bf858a40a6af56445c5bb50
Fukada_Ryuto <144636715+SunagimoOisii@users.noreply.github.com>
2025-03-17 16:28:54 +0900

    Update Button_EscapeGame.cs

--- Assets/Scripts/Button/Button_EscapeGame.cs
+++ Assets/Scripts/Button/Button_EscapeGame.cs
@@ -5,7 +5,7 @@ public class Button_EscapeGame : MonoBehaviour, IPointerDownHandler
 {
     public void OnPointerDown(PointerEventData eventData)
     {
-        //ƒQ[ƒ€I—¹ˆ—
+        //ã‚²ãƒ¼ãƒ çµ‚äº†å‡¦ç†
         #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         #else
